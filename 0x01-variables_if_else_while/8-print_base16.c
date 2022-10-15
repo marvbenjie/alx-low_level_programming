958f9bbade8ff0ddb9c0bb3f71808de84bfbaec9
marvbenjie <marvbenjie100@gmail.com>
2022-10-15 08:31:04 -0700

    Corrections

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,14 +10,14 @@
 int main(void)
 {
 	char c;
-	char d;
+	char d = '0';
 
-	while(d <= '9')
+	while (d <= '9')
 	{
 		putchar(d);
 		d++;
 	}
-	for(c = 'a';c <= 'f';c++)
+	for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar(c);
 	}
