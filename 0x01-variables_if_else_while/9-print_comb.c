8f4f5fa7d41de91f5bac49480bcdd27c79e7ca60
marvbenjie <marvbenjie100@gmail.com>
2022-10-15 08:34:40 -0700

    Corrections

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,13 @@ int main(void)
 {
 	int i;
 
-	for(i = '0';i <= '9';i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
 
-		if(i != '9')
+		if (i != '9')
 		{
-			putchar(';');
+			putchar(',');
 			putchar(' ');
 		}
 	}
